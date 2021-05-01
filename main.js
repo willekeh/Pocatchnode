421a9277318a1a67dffe9cf779452cebbc1411da
rianneh <58731528+rianneh@users.noreply.github.com>
2021-05-01 23:43:34 +0200

    pokedex

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
 const Discord = require('discord.js');
+const mysql = require('mysql');
 const Token = require('./config.json');
+
+const pk = require(`./pokemon/gen1.js`)
+const pkdx = require('./pokedexentry/gen1dx.js')
 
 const client = new Discord.Client();
 
-const prefix ='!';
+const prefix ='$';
 
-var check = true;
+var check = false;
+let random = 0;
+
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "pocatch"
+})
+
+con.connect(err => {
+    if(err) throw err;
+    console.log("Connected to the database");
+})
 
 client.once('ready', () => {
     console.log('Pocatch is online :D');
@@ -21,35 +39,60 @@ client.on('message', message => {
 
         check = true;
 
-        message.channel.send('you see a pokemon in the distance, but what is it. use !pokeball to try and catch it ');
+        message.channel.send('you see a pokemon in the distance, but what is it. use $pokeball to try and catch it ');
 
-        return check
+        random = Math.ceil(Math.random() * (151));
+        console.log(pk.gen1[random])
+
+        return check && random
 
     } else if(command === 'pokeball'){
 
         if(check){
-            // check = false;
+            check = false;
         
             const broken = ["0", "You didnt even have a wiggle", "You almost had it but it broke free :("];
         
             const randomBroken = broken[Math.floor(Math.random() * broken.length)];
             if (randomBroken === "0"){
-                const messages = ["You caught Munchlax", "You caught Pikachu", "You caught Eevee", "You caught Charmander"];
 
-                const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+                const messages = pk.gen1[random];
 
-                message.channel.send(randomMessage);
-                console.log(randomMessage)
+                message.channel.send(messages);
+                console.log(messages);
+                random = 0;
 
-            }else {
+            } else {
                 message.channel.send(randomBroken);
-                console.log(randomBroken)
+                console.log(randomBroken);
+                random = 0;
         }
 
         }else {
-            message.channel.send("There is no pokemon to be found. try !pokemon");
+            message.channel.send("There is no pokemon to be found. try $pokemon");
         }
 
+    } else if(command === 'pokedex'){
+
+        if(check) {
+            // if(npkdx = True)
+
+            con.query("SELECT * FROM pokedex WHERE pokemon_id = 1", (err, results) => {
+                if(err) throw err;
+
+                let randomdex = Math.ceil(Math.random() * (results.length))
+
+                let entry = results[randomdex].entry
+
+                console.log(entry)
+
+                message.channel.send(entry); 
+                
+            });
+
+        } else {
+            message.channel.send('There is no pokemon. try $pokemon');
+        }
     }
 });
 
